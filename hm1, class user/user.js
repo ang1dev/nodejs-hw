b57d2fc236ebb2cdcace172b5f0615416ad794be
anevceva <anevcheva@gmail.com>
2023-03-06 21:00:17 +0100

    homework 1

---
+++ hm1, class user/user.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+
+
+function create(path, data) {
+    fs.writeFileSync(path, JSON.stringify(data));
+}
+
+function append(path, data) {
+    fs.appendFileSync(path, JSON.stringify(data))
+}
+
+function registerDate(path,userArray) {
+    for (let i = 0; i < userArray.length; i++) {
+        if (userArray[i].dateCreated == "13.02.23") {
+            userArray[i].isActive = false            
+        }
+    }
+    fs.appendFileSync(path, JSON.stringify(userArray));
+}
+
+function deleteUser(path,username) {
+    let data = fs.readFileSync("users.json", { encoding: "utf-8" });
+    let parsedData = JSON.parse(data)
+
+    for (let i = 0; i < parsedData.length; i++) {
+        if (parsedData[i].username == username) {
+            const index = parsedData.indexOf(parsedData[i]);
+            if (index > -1) { 
+                parsedData.splice(index, 1); 
+            }
+        }
+    }
+    fs.writeFileSync(path, JSON.stringify(parsedData));
+}
+
+function inActive(path) {
+    let data = fs.readFileSync(path, { encoding: "utf-8" });
+    let parsedData = JSON.parse(data);
+    for (let i = 0; i < parsedData.length; i++) {
+        if (parsedData[i].isActive == false) {
+            const index = parsedData.indexOf(parsedData[i]);
+            if (index > -1) {
+                parsedData.splice(index, 1);
+            }
+        }
+    }
+
+    fs.writeFileSync(path, JSON.stringify(parsedData));
+}
+
+module.exports = { create, append, registerDate, deleteUser, inActive };
